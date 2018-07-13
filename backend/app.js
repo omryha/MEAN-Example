3f9a7f2adbfbc30142f68945bf5dbdb050d92613
Omry Hazut <omry.hazut@mail.huji.ac.il>
2018-07-13 15:43:03 +0200

    deleting posts fix

--- backend/app.js
+++ backend/app.js
@@ -25,9 +25,11 @@ app.post('/api/posts', (req, res, next) => {
         title: req.body.title,
         content: req.body.content
     });
-    post.save();
-    res.status(201).json({
-        message: 'Post added successfully'
+    post.save().then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            postID: createdPost.id
+        });
     }); // 201 = OK, new resource was created
 });
 
