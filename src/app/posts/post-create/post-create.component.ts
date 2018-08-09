6a0f91709fab29befe1deafc76bb30ca6cac08da
Omry Hazut <omry.hazut@mail.huji.ac.il>
2018-08-09 23:33:40 +0200

    Edit post fix

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,12 +1,11 @@
-import { PostsService } from '../posts.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
 import { mimeType } from './mime-type.validator';
-import { Subscription } from '../../../../node_modules/rxjs';
 import { AuthService } from '../../auth/auth.service';
-import { THIS_EXPR } from '../../../../node_modules/@angular/compiler/src/output/output_ast';
 
 @Component({
     selector: 'app-post-create',
@@ -14,17 +13,19 @@ import { THIS_EXPR } from '../../../../node_modules/@angular/compiler/src/output
     styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit, OnDestroy {
-    private mode = 'create';
-    private postId: string;
-    private authStatusSub: Subscription;
-    enteredContent = '';
     enteredTitle = '';
+    enteredContent = '';
     post: Post;
     isLoading = false;
     form: FormGroup;
     imagePreview: string;
+    private mode = 'create';
+    private postId: string;
+    private authStatusSub: Subscription;
 
-    constructor(public postsService: PostsService, public route: ActivatedRoute, private authService: AuthService) { }
+    constructor(public postsService: PostsService,
+        public route: ActivatedRoute,
+        private authService: AuthService) { }
 
     ngOnInit() {
         this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
@@ -50,31 +51,17 @@ export class PostCreateComponent implements OnInit, OnDestroy {
                         imagePath: postData.imagePath,
                         creator: postData.creator
                     };
+                    this.form.setValue({
+                        title: this.post.title,
+                        content: this.post.content,
+                        image: this.post.imagePath
+                    });
                 });
-                this.form.setValue({ 'title': this.post.title, 'content': this.post.content, image: this.post.imagePath });
             } else {
                 this.mode = 'create';
                 this.postId = null;
             }
         });
-    }
-
-    onSavePost() {
-        if (this.form.invalid) {
-            return;
-        }
-        this.isLoading = true;
-        if (this.mode === 'create') {
-            this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
-        } else {
-            this.postsService.updatePost(
-                this.postId,
-                this.form.value.title,
-                this.form.value.content,
-                this.form.value.image
-            );
-        }
-        this.form.reset(); // Also resets the form Validity
     }
 
     onImagePicked(event: Event) {
@@ -86,6 +73,28 @@ export class PostCreateComponent implements OnInit, OnDestroy {
             this.imagePreview = reader.result;
         };
         reader.readAsDataURL(file);
+    }
+
+    onSavePost() {
+        if (this.form.invalid) {
+            return;
+        }
+        this.isLoading = true;
+        if (this.mode === 'create') {
+            this.postsService.addPost(
+                this.form.value.title,
+                this.form.value.content,
+                this.form.value.image
+            );
+        } else {
+            this.postsService.updatePost(
+                this.postId,
+                this.form.value.title,
+                this.form.value.content,
+                this.form.value.image
+            );
+        }
+        this.form.reset(); // Also resets the form Validity
     }
 
     ngOnDestroy() {
